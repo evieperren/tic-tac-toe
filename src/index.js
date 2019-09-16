569776874c917acf8a6cf60a4117de6795b94f4e
Evie Perren <evie.perren@simplyhealth.co.uk>
2019-09-16 21:50:20 +0100

    console out 9 items

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 const playerOne = $('.player-one');
 const playerTwo = $('.player-two');
 
+
 // check if DOM is ready 
 $(document).ready(function(){
     // alert('document loaded');
 
     // highlight who is playing
     playerOne.click(function(){
-        console.log('i was clicked!')
         playerOne.addClass('player-one--selected');
+        playerTwo.removeClass('player-two--selected');
     })
     playerTwo.click(function () {
-        console.log('i was clicked!')
         playerTwo.addClass('player-two--selected');
+        playerOne.removeClass('player-one--selected');
+        
     })
 
     // change icon displayed dependant on what is selected
@@ -22,57 +24,35 @@ $(document).ready(function(){
     } else if (playerTwo.hasClass('player-two--selected')){
         console.log('player two is selected');
     }
-    
-    // could be a for loop
-    $('#2').click(function () {
-        $('#2').html('X');
-        // $('#2').click(function () {
-        //     $('#2').html('')
-        // });
-    });
-    $('#3').click(function () {
-        $('#3').html('X');
-        // $('#3').click(function () {
-        //     $('#3').html('')
-        // });
-    });
-    $('#4').click(function () {
-        $('#4').html('X');
-        // $('#4').click(function () {
-        //     $('#4').html('')
-        // });
-    });
-    $('#5').click(function () {
-        $('#5').html('X');
-        // $('#5').click(function () {
-        //     $('#5').html('')
-        // });
-    });
-    $('#6').click(function () {
-        $('#6').html('X');
-        // $('#6').click(function () {
-        //     $('#6').html('')
-        // });
-    });
-    $('#7').click(function () {
-        $('#7').html('X');
-        // $('#7').click(function () {
-        //     $('#7').html('')
-        // })
-    });
-    $('#8').click(function () {
-        $('#8').html('X');
-        // $('#8').click(function () {
-        //     $('#8').html('')
-        // })
-    });
-    $('#9').click(function () {
-        $('#9').html('X');
-        // $('#9').click(function () {
-        //     $('#9').html('')
-        // })
-    });
 
+    // allFields.each(function (field) {
+    //     allFields.click( function () {
+    //         console.log('clicked!');
+    //         for(let i = 0; i < allFields.length; i++){
+    //             let field =  allFields[i].value;
+    //             console.log(field)
+    //         }
+    //     });
+    // });
+    const allFields = $('.game__table--item');
+    allFields.each(function(i, item){
+        for (i = 0; i < allFields.length; i++) {
+            
+            let field = allFields.eq[i];
+            field = '';
+            console.log(field);
+            return field;
+        }
+    })
+
+
+    // could be a for loop
+    // $('#2').click(function () {
+    //     $('#2').html('X');
+    //     // $('#2').click(function () {
+    //     //     $('#2').html('')
+    //     // });
+    // });
 
     // restart the board 
     $('.restart').click(function(){
