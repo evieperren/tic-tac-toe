2623d5a7e596e292b6320531327bd64289a6aafe
Evie Perren <evie.perren@simplyhealth.co.uk>
2019-09-17 10:14:04 +0100

    click only one item in list

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ $(document).ready(function(){
         playerTwo.removeClass('player-two--selected');
         
         box.click(function(){ 
-            for (let i = 0; i < box.length; i++){
-                let boxItem = box.eq[i];
-                boxItem = 'x';
-                console.log(boxItem);
-                box[i].replaceWith(boxItem);
-            }
+            console.log(this);
+            box.html = 'x';
+            // for (let i = 0; i < box.length; i++){
+            //     let boxItem = box.eq[i];
+            //     box[i].replaceWith(boxItem);
+            // }
         })
     });
 
@@ -30,7 +30,6 @@ $(document).ready(function(){
     playerTwo.click(function () {
         playerTwo.addClass('player-two--selected');
         playerOne.removeClass('player-one--selected');
-        field = 'o';
     })
 
     $('.restart').click(function(){
