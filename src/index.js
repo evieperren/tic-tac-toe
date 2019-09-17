82de714a8d8de2a1b5079078be3e0aa4fb35fa32
Evie Perren <evie.perren@simplyhealth.co.uk>
2019-09-17 10:55:56 +0100

    change player

--- src/index.js
+++ src/index.js
@@ -2,40 +2,39 @@ const playerOne = $('.player-one');
 const playerTwo = $('.player-two');
 const box = $('.game__table--item');
 
-// check if DOM is ready 
 $(document).ready(function(){
 
     playerOne.click(function(){
         playerOne.addClass('player-one--selected');
         playerTwo.removeClass('player-two--selected');
         
-        box.click(function(){ 
-            console.log(this);
-            box.html = 'x';
-            // for (let i = 0; i < box.length; i++){
-            //     let boxItem = box.eq[i];
-            //     box[i].replaceWith(boxItem);
-            // }
-        })
     });
 
-// only returns first item? 
-    for (let i = 0; i < box.length; i++) {
-        let boxItem = box[i];
-        // console.log(boxItem);
-        return boxItem;
-    };
-    
-// no longer is registered?
     playerTwo.click(function () {
         playerTwo.addClass('player-two--selected');
         playerOne.removeClass('player-one--selected');
-    })
 
+    });
+
+    box.click(function(){ 
+
+        if (playerOne.hasClass('player-one--selected')){
+            console.log('x should show');
+            console.log(this);
+            playerTwo.addClass('player-two--selected');
+            playerOne.removeClass('player-one--selected');
+
+        } else if (playerTwo.hasClass('player-two--selected')){
+            console.log('o should show');
+            console.log(this);
+            playerOne.addClass('player-one--selected');
+            playerTwo.removeClass('player-two--selected');
+
+        } return;
+    });
+     
     $('.restart').click(function(){
-        box.each(function(){
-            field = '';
-        })
+       console.clear();
     })
 
 })
