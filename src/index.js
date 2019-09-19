869e0ff69535e7706c47a69789ab59634db8cc12
Evie Perren <evie.perren@simplyhealth.co.uk>
2019-09-19 10:14:21 +0100

    making changes to rules

--- src/index.js
+++ src/index.js
@@ -16,79 +16,64 @@ $(document).ready(function(){
     playerTwo.click(teUpPlayertwo());
 
     box.click(function() { 
-        // let text = (playerOne.hasClass('player-one--selected') ? $(this).text('X') : $(this).text('O'));
         if (playerOne.hasClass('player-one--selected')){
             $(this).text('X');
-            $(this).addClass('cross');
             teUpPlayertwo();
-
-
-           // console.log(box.find('#1').hasClass('cross')); // returns false
-            // if (!box.find('#1').hasClass('cross') && !box.find('#2').hasClass('cross')) {
-            //     console.log('cross wins');
-            // }
-
+        
         } else if (playerTwo.hasClass('player-two--selected')){
             $(this).text('O');
-            $(this).addClass('naught');
-            teUpPlayerOne(); 
+            teUpPlayerOne();
         }
+
+        if(findWinner() != -1 && findWinner() != ""){
+            if(findWinner() === 'X'){
+                playerOne.append('<p class="player-one--score">Winner :)</p>');
+                return
+            } else  if (findWinner() === 'O'){
+                playerTwo.append('<p class="player-two--score">Winner :)</p>');
+                return
+            } else {
+                $('body').append('<div>Draw</div>')
+            }
+         }
     });
-    
-    let winner = 0;
-    
-    if (box.find('#1').hasClass('cross')) {
-        winner = 1;
-        console.log(winner);
+    function findWinner(){
+        boxes = {
+            item1: $('#1').text(),
+            item2: $('#2').text(),
+            item3: $('#3').text(),
+            item4: $('#4').text(),
+            item5: $('#5').text(),
+            item6: $('#6').text(),
+            item7: $('#7').text(),
+            item8: $('#8').text(),
+            item9: $('#9').text(),
+        }
+        matches = {
+            match1: boxes.item1 == boxes.item2 && boxes.item2 == boxes.item3
+        }
+        console.log(matches.match1);
+        if ((boxes.item1 == boxes.item2) && (boxes.item2 == boxes.item3)){
+            return boxes.item3;
+        } else if ((boxes.item4 == boxes.item5) && (boxes.item5 == boxes.item6)){
+            return boxes.item6;
+        } else if ((boxes.item7 == boxes.item8) && (boxes.item8 == boxes.item9)) {
+            return boxes.item9;
+        } else if ((boxes.item1 == boxes.item4) && (boxes.item4 == boxes.item7)){
+            return boxes.item7;
+        } else if ((boxes.item2 == boxes.item5) && (boxes.item5 == boxes.item8)) {
+            return boxes.item8;
+        } else if ((boxes.item3 == boxes.item6) && (boxes.item6 == boxes.item9)) {
+            return boxes.item9;
+        } else if ((boxes.item1 == boxes.item5) && (boxes.item5 == boxes.item9)) {
+            return boxes.item9;
+        } else if ((boxes.item3 == boxes.item5) && (boxes.item5 == boxes.item7)) {
+            return boxes.item7;
+        }   return -1;
     }
     
     $('.restart').click(function(){
-       console.clear();
-       box.text('');
+        console.clear();
+        box.text('');
     });
-
-    
-    //check winner
-    
 });
-
-// let xWinRule1 = $(['#1', '#2', '#3']).length === 3;
-// let xWinRule2 = $(['#4', '#5', '#6']).length === 3;
-// let xWinRule3 = $(['#7', '#8', '#9']).length === 3;
-// let xWinRule4 = $(['#1', '#4', '#7']).length === 3;
-// let xWinRule5 = $(['#2', '#5', '#8']).length === 3;
-// let xWinRule6 = $(['#3', '#6', '#9']).length === 3;
-// let xWinRule7 = $(['#1', '#5', '#8']).length === 3;
-// let xWinRule8 = $(['#3', '#5', '#7']).length === 3;
-
-// let oWinRule1 = $(['#1', '#2', '#3']).length === 3;
-// let oWinRule2 = $(['#4', '#5', '#6']).length === 3;
-// let oWinRule3 = $(['#7', '#8', '#9']).length === 3;
-// let oWinRule4 = $(['#1', '#4', '#7']).length === 3;
-// let oWinRule5 = $(['#2', '#5', '#8']).length === 3;
-// let oWinRule6 = $(['#3', '#6', '#9']).length === 3;
-// let oWinRule7 = $(['#1', '#5', '#8']).length === 3;
-// let oWinRule8 = $(['#3', '#5', '#7']).length === 3;
-
-// console.log(oWinRule2); // returns true
-
-// let xWins = (xWinRule1 || xWinRule2 || xWinRule3 || xWinRule4 || xWinRule5 || xWinRule6 || xWinRule7 || xWinRule8);
-// let oWins = (oWinRule1 || oWinRule2 || oWinRule3 || oWinRule4 || oWinRule5 || oWinRule6 || oWinRule7 || oWinRule8);
-
-// if(xWins){
-//     console.log('x wins');
-// } else if (oWins){
-//     console.log('o wins');
-// } else {
-//     console.log('no winner');
-// }  
-
-// }
-// 1, 2, 3
-// 4, 5, 6
-// 7, 8, 9
-// 1, 4, 7
-// 2, 5, 8
-// 3, 6, 9
-// 1, 5, 9
-// 3, 5, 7
